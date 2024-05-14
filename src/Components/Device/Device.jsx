0ba08f2751fbb4a48c2bcb1b7596a15825bd0f49
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-05-14 18:06:07 +0600

    selected products added

--- src/Components/Device/Device.jsx
+++ src/Components/Device/Device.jsx
@@ -1,17 +1,19 @@
+
 import './Device.css'
 
-const Device = ({device}) => {
-    const {name,price,img,ratings} =device;
+
+const Device = ({ device ,handleAddToCart }) => {
+    const { name, price, img, ratings,id} = device;
     return (
         <div className="product-style">
             {
-         <img src={img} />
-       }
+                <img src={img} />
+            }
             <h3>{name}</h3>
             <h5>Price :{price}</h5>
             <p><small>Ratings : {ratings}</small></p>
-            <button style={{margin:'10px',color:'green', background:'skyblue'}}>Purchase</button>
-       
+            <button onClick={()=>{handleAddToCart(id)}} style={{ margin: '10px', color: 'green', background: 'skyblue' }}>Purchase</button>
+
         </div>
     );
 };
--- src/Components/Devices/Devices.jsx
+++ src/Components/Devices/Devices.jsx
@@ -6,24 +6,38 @@ import './Devices.css'
 
 
 const Devices = () => {
-    const [devices, setDivices] =useState([]);
+    const [devices, setDivices] = useState([]);
+    const [cart, setCart]= useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('DemoData.json')
-        .then(res => res.json())
-        .then(data => setDivices(data));
-    },[])
+            .then(res => res.json())
+            .then(data => setDivices(data));
+    }, [])
+
+    const handleAddToCart = (id) => {
+        console.log('cart added');
+        const newCart = [...cart,id];
+        setCart(newCart);
+        console.log(newCart)
+        
+    }
     return (
         <div >
-            <h2 style={{padding:'15px'}}>Total Devices {devices.length}</h2>
+            <h2 style={{ padding: '15px' }}>Total Devices {devices.length}</h2>
+            <h4 style={{ padding: '15px' }}  >Selected Products : {cart.length}</h4>
             <div className="products-container">
-                 {
-                devices.map(device => <Device device={device}></Device> )
-            }
+                {
+                    devices.map(device => <Device 
+                        device={device}
+                      
+                        handleAddToCart={handleAddToCart}
+                        ></Device>)
+                }
 
             </div>
-            
-           
+
+
         </div>
     );
 };
